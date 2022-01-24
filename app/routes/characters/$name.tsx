d645342a3d0bf83c2244d1c13a0008a0eb28a931
Jody Geraldo <ajody.geraldo@gmail.com>
2022-01-24 15:55:09 +0700

    clean up

--- app/routes/characters/$name.tsx
+++ app/routes/characters/$name.tsx
@@ -1,134 +1,22 @@
 import {
-  ActionFunction,
   json,
-  Link,
   LoaderFunction,
   Outlet,
-  redirect,
   useCatch,
   useLoaderData,
-  useMatches,
   useOutletContext,
   useParams,
 } from 'remix'
 import invariant from 'tiny-invariant'
 
-import CharacterLevel from '~/components/Character/CharacterLevel/CharacterLevel'
-import CharacterLevelManual from '~/components/Character/CharacterLevel/CharacterLevelManual/CharacterLevelManual'
-import CharacterView from '~/components/Character/CharacterView'
 import { characters } from '~/data/characters.server'
 import type { CharacterName, ICharacter, ITraveler } from '~/types/character'
-import { CharacterActionTypeEnum } from '~/types/character'
 import { requireUserSession } from '~/utils/auth.server'
 import {
   getUserCharacter,
   getUserCharacterOwnership,
-  parseTalentToNumberArray,
-  removeUserCharacterOwnershipEntry,
-  setUserCharacter,
-  setUserCharacterOwnership,
 } from '~/utils/character.server'
-import { getFormHackMessage } from '~/utils/message'
-import type { User } from '~/utils/redis/redis-user-schema.server'
 import { stringToCapitalized } from '~/utils/string'
-import { getDataId, getUserData } from '~/utils/user.server'
-import { commitSession, getUserDataSession } from '~/utils/user-data.server'
-
-export const action: ActionFunction = async ({ request }) => {
-  const user = await requireUserSession(request)
-
-  const formData = await request.formData()
-  const _action = formData.get('_action')
-  const name = formData.get('character-name')
-  const level = formData.get('character-level')
-  const ascension = formData.get('character-ascension')
-  const ownership = formData.get('owned')
-  const talentNormal = formData.get('talent-normal')
-  const talentSkill = formData.get('talent-skill')
-  const talentBurst = formData.get('talent-burst')
-
-  const userDataSession = await getUserDataSession(request)
-  const userDataIds = userDataSession.get('userData') as User['data_ids']
-
-  invariant(typeof _action === 'string', getFormHackMessage())
-  invariant(typeof name === 'string', getFormHackMessage())
-
-  switch (_action as CharacterActionTypeEnum) {
-    case CharacterActionTypeEnum.Ownership: {
-      invariant(typeof ownership === 'string', getFormHackMessage())
-
-      const ownershipDataId = getDataId(userDataIds, 'characterOwnership')
-      if (ownership === 'true') {
-        const id = await setUserCharacterOwnership(
-          name,
-          user.id,
-          ownershipDataId,
-        )
-
-        if (!id) {
-          return json(null, { status: 200 })
-        }
-
-        const userData = await getUserData(user.id)
-        userDataSession.set('userData', userData)
-
-        return json(request.url, {
-          status: 201,
-          headers: { 'Set-Cookie': await commitSession(userDataSession) },
-        })
-      }
-
-      // this is should have never happened unless data not synced properly with the database
-      // TODO: throw some error here IDK what for now
-      if (!ownershipDataId) {
-        return json(null, { status: 400 })
-      }
-
-      await removeUserCharacterOwnershipEntry(name, ownershipDataId)
-
-      return json(null, { status: 200 })
-    }
-    case CharacterActionTypeEnum.Munual: {
-      invariant(typeof level === 'string', getFormHackMessage())
-      invariant(typeof ascension === 'string', getFormHackMessage())
-      invariant(typeof talentNormal === 'string', getFormHackMessage())
-      invariant(typeof talentSkill === 'string', getFormHackMessage())
-      invariant(typeof talentBurst === 'string', getFormHackMessage())
-
-      const characterDataId = getDataId(userDataIds, name)
-
-      const talent = parseTalentToNumberArray(
-        talentNormal,
-        talentSkill,
-        talentBurst,
-      )
-
-      const id = await setUserCharacter(
-        name,
-        +level,
-        +ascension,
-        talent,
-        user.id,
-        characterDataId,
-      )
-
-      if (!id) {
-        return json(null, { status: 200 })
-      }
-
-      const userData = await getUserData(user.id)
-      userDataSession.set('userData', userData)
-
-      return json(request.url, {
-        status: 201,
-        headers: { 'Set-Cookie': await commitSession(userDataSession) },
-      })
-    }
-
-    default:
-      return json(null, { status: 204 })
-  }
-}
 
 type LoaderData = {
   name: CharacterName
