cd8bd4de6d5361e6ac384794fa97b4c8cf94c61c
Jody Geraldo <ajody.geraldo@gmail.com>
2022-01-29 20:08:21 +0700

    add route per category

--- app/routes/inventory.tsx
+++ app/routes/inventory.tsx
@@ -1,9 +1,12 @@
+import clsx from 'clsx'
 import {
   json,
   LoaderFunction,
+  NavLink,
   Outlet,
   ShouldReloadFunction,
   useLoaderData,
+  useLocation,
 } from 'remix'
 
 import { requireUserId } from '~/services/auth.server'
@@ -29,12 +32,45 @@ export const loader: LoaderFunction = async ({ request }) => {
   return json<ItemTypes>(updatedItems, { status: 200 })
 }
 
+const navigation = [
+  { name: 'All', to: '' },
+  { name: 'Common Material', to: 'common-material' },
+  { name: 'Talent Boss Material', to: 'talent-boss-material' },
+  { name: 'Ascension Boss Material', to: 'ascension-boss-material' },
+  { name: 'Ascension Gem', to: 'ascension-gem' },
+  { name: 'Talent Book', to: 'talent-book' },
+  { name: 'Crown', to: 'crown' },
+  { name: 'Local Specialty', to: 'local-specialty' },
+]
+
 export default function InventoryPage() {
   const items = useLoaderData<ItemTypes>()
+  const location = useLocation()
+  // IDK why typescript think this is only can be a string
+  const currentPathname = location.pathname.split('/')[2] as string | undefined
 
-  return <Outlet context={items} />
-}
-
-export const unstable_shouldReload: ShouldReloadFunction = () => {
-  return false
+  return (
+    <div className="flex gap-8">
+      <nav className="sticky top-5 h-full space-y-1 " aria-label="Sidebar">
+        {navigation.map(item => (
+          <NavLink
+            key={item.name}
+            to={item.to}
+            className={({ isActive }) =>
+              clsx(
+                (item.to === currentPathname && isActive) ||
+                  (currentPathname === undefined && item.to === '')
+                  ? 'bg-gray-800 text-gray-100'
+                  : 'text-gray-600 hover:bg-gray-800 hover:text-gray-100',
+                'flex items-center rounded-md px-3 py-2 text-sm font-medium',
+              )
+            }
+          >
+            <span className="truncate">{item.name}</span>
+          </NavLink>
+        ))}
+      </nav>
+      <Outlet context={items} />
+    </div>
+  )
 }
---
+++ app/routes/inventory/common-material.tsx
@@ -0,0 +1,58 @@
+import { InformationCircleIcon } from '@heroicons/react/outline'
+import { ActionFunction, useOutletContext } from 'remix'
+import invariant from 'tiny-invariant'
+
+import Item from '~/components/Item/Item'
+import { requireUserId } from '~/services/auth.server'
+import { db } from '~/services/db.server'
+import { ItemTypes } from '~/types/item'
+import { upsertCommonMaterial } from '~/utils/db/item.server'
+
+export const action: ActionFunction = async ({ request }) => {
+  const userId = await requireUserId(request)
+
+  const formData = await request.formData()
+
+  const category = formData.get('category')
+  const name = formData.get('name')
+  const count = formData.get('count')
+
+  invariant(typeof category === 'string', 'category must be a string')
+  invariant(typeof name === 'string', 'name must be a string')
+  invariant(typeof count === 'string', 'count must be a string')
+
+  const userItem = await db.item.findUnique({
+    where: {
+      userId,
+    },
+  })
+
+  if (category === 'Common Material') {
+    // TODO: add trycatch to handle fail to upsert
+    await upsertCommonMaterial(userId, userItem, name, +count)
+  }
+
+  return null
+}
+
+export default function InventoryCommonMaterialRoute() {
+  const items = useOutletContext<ItemTypes>()
+  const filteredItems = items.filter(item => item.name === 'Common Material')
+
+  return (
+    <div>
+      {filteredItems.map(item => (
+        <div key={item.name} className="mb-8 space-y-4 last-of-type:mb-0">
+          <div className="flex items-center gap-2 rounded-md border-2 border-slate-500 bg-slate-900 p-2">
+            <InformationCircleIcon className="h-6 w-6 text-yellow-400" />
+            <p className="text-slate-200">
+              Use <span className="text-teal-400">CTRL + F</span> to search
+              items
+            </p>
+          </div>
+          <Item category={item.name} item={item.items} />
+        </div>
+      ))}
+    </div>
+  )
+}
