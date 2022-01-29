cf53609a293b99ee19dff3fb0a462269b446dc36
Jody Geraldo <ajody.geraldo@gmail.com>
2022-01-29 15:48:05 +0700

    remove check for og 7

--- app/components/character/CharacterView.tsx
+++ app/components/character/CharacterView.tsx
@@ -1,43 +1,38 @@
 import type { FC } from 'react'
 import { Form, useTransition } from 'remix'
 
-import { CharacterName, ICharacter, ITraveler } from '~/types/character'
+import { ICharacter } from '~/types/character'
 
 interface Props {
-  character: ICharacter | ITraveler
+  character: ICharacter
 }
-
-const og7character: Array<CharacterName> = [
-  'Traveler',
-  'Amber',
-  'Noelle',
-  'Lisa',
-  'Kaeya',
-  'Barbara',
-  'Xiangling',
-]
 
 const CharacterView: FC<Props> = ({ character }) => {
   const transition = useTransition()
-  const id = character.dbId ?? 'NEW'
+  const id = character.id ?? 'NEW'
 
   return (
     <div className="flex items-start justify-between">
       <h1 className="text-6xl">{character.name}</h1>
-      {og7character.includes(character.name) ? null : (
-        <Form replace method="post">
-          <input type="hidden" name="id" value={id} />
-          <input type="hidden" name="name" value={character.name} />
-          <input type="hidden" name="toggle-own" value={`${!character.own}`} />
-          <button className="rounded-md bg-primary-400 px-4 py-2 shadow-md shadow-primary-800 ">
-            {transition.submission
-              ? 'Saving...'
-              : character.own
-              ? 'Mark as Unowned'
-              : 'Mark as Owned'}
-          </button>
-        </Form>
-      )}
+      <Form replace method="post">
+        <input type="hidden" name="id" value={id} />
+        <input type="hidden" name="name" value={character.name} />
+        <input
+          type="hidden"
+          name="toggle-own"
+          value={`${!character.ownership}`}
+        />
+        <button
+          disabled={transition.state === 'submitting'}
+          className="bg-primary-400 shadow-primary-800 rounded-md px-4 py-2 shadow-md "
+        >
+          {transition.submission
+            ? 'Saving...'
+            : character.ownership
+            ? 'Mark as Unowned'
+            : 'Mark as Owned'}
+        </button>
+      </Form>
     </div>
   )
 }
