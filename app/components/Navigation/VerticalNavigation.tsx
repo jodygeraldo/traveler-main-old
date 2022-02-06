b0ede4646968d5c5ffc4be1400c969e3319bcd24
Jody Geraldo <ajody.geraldo@gmail.com>
2022-02-06 19:31:48 +0700

    navigation component

---
+++ app/components/Navigation/VerticalNavigation.tsx
@@ -0,0 +1,29 @@
+import clsx from 'clsx'
+import { Link } from 'remix'
+
+type Navigation = {
+  name: string
+  to: string
+  current: boolean
+}[]
+
+export default function VerticalNavigation({ items }: { items: Navigation }) {
+  return (
+    <nav className="space-y-1">
+      {items.map((item) => (
+        <Link
+          key={item.name}
+          to={item.to}
+          className={clsx(
+            item.current
+              ? 'bg-gray-50 text-orange-600 hover:bg-white'
+              : 'text-gray-900 hover:bg-gray-50 hover:text-gray-900',
+            'group flex items-center rounded-md px-3 py-2 text-sm font-medium'
+          )}
+          aria-current={item.current ? 'page' : undefined}>
+          {item.name}
+        </Link>
+      ))}
+    </nav>
+  )
+}
