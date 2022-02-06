ac6542d5c04cdd9bc5d61177547d96b820a2c11c
Jody Geraldo <ajody.geraldo@gmail.com>
2022-02-06 19:31:55 +0700

    apps layout

---
+++ app/components/AppsLayout.tsx
@@ -0,0 +1,170 @@
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import clsx from 'clsx'
+import { Fragment } from 'react'
+import { Link } from 'remix'
+
+const navigation = [
+  { name: 'Handbook', href: '/handbook' },
+  { name: 'Character', href: '/character' },
+  { name: 'Inventory', href: '/inventory' },
+]
+const userNavigation = [
+  { name: 'Your Profile', href: '/profile' },
+  { name: 'Settings', href: '/settings' },
+  { name: 'Sign out', href: '/signout' },
+]
+
+export default function AppsLayout({
+  email,
+  children,
+}: {
+  email: string
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <Disclosure as="header" className="bg-white shadow">
+        {({ open }) => (
+          <>
+            <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:divide-y lg:divide-gray-200 lg:px-8">
+              <div className="relative flex h-16 justify-between">
+                <div className="relative z-10 flex px-2 lg:px-0">
+                  <div className="flex flex-shrink-0 items-center">
+                    <span className="text-xl font-semibold text-orange-600 leading-5 sm:text-2xl">
+                      Traveler Main
+                    </span>
+                    {/* <img
+                      className="block h-8 w-auto"
+                      src="https://tailwindui.com/img/logos/workflow-mark-orange-500.svg"
+                      alt="Workflow"
+                    /> */}
+                  </div>
+                </div>
+                <div className="relative z-10 flex items-center lg:hidden">
+                  {/* Mobile menu button */}
+                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-900">
+                    <span className="sr-only">Open menu</span>
+                    {open ? (
+                      <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
+                <div className="hidden lg:relative lg:z-10 lg:ml-4 lg:flex lg:items-center">
+                  {/* <button
+                      type="button"
+                      className="flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2">
+                      <span className="sr-only">View notifications</span>
+                      <BellIcon className="h-6 w-6" aria-hidden="true" />
+                    </button> */}
+
+                  {/* Profile dropdown */}
+                  <Menu as="div" className="relative ml-4 flex-shrink-0">
+                    <div>
+                      <Menu.Button className="flex bg-white">
+                        <span className="sr-only">Open user menu</span>
+                        <span>Hi,&nbsp;</span>
+                        <span className="text-orange-600 underline underline-offset-4 decoration-dotted">{`${email}`}</span>
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95">
+                      <Menu.Items className="absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        {userNavigation.map((item) => (
+                          <Menu.Item key={item.name}>
+                            {({ active }) => (
+                              <Link
+                                to={item.href}
+                                className={clsx(
+                                  active ? 'bg-gray-100' : '',
+                                  'block py-2 px-4 text-sm text-gray-700'
+                                )}>
+                                {item.name}
+                              </Link>
+                            )}
+                          </Menu.Item>
+                        ))}
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                </div>
+              </div>
+              <nav
+                className="hidden lg:flex lg:space-x-8 lg:py-2"
+                aria-label="Global">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    to={item.href}
+                    className="inline-flex items-center rounded-md py-2 px-3 text-sm font-medium text-gray-900 hover:bg-gray-50 hover:text-gray-900">
+                    {item.name}
+                  </Link>
+                ))}
+              </nav>
+            </div>
+
+            <Disclosure.Panel
+              as="nav"
+              className="lg:hidden"
+              aria-label="Global">
+              <div className="space-y-1 px-2 pt-2 pb-3">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as={Link}
+                    to={item.href}
+                    className="block rounded-md py-2 px-3 text-base font-medium text-gray-900 hover:bg-gray-50 hover:text-gray-900">
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+              <div className="border-t border-gray-200 pt-4 pb-3">
+                <div className="flex items-center px-4">
+                  <div className="text-base font-medium text-gray-800 px-1">
+                    <span>Hi,&nbsp;</span>
+                    <span className="text-orange-600 underline underline-offset-4 decoration-dotted">{`${email}`}</span>
+                  </div>
+                  {/* <button
+                      type="button"
+                      className="ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2">
+                      <span className="sr-only">View notifications</span>
+                      <BellIcon className="h-6 w-6" aria-hidden="true" />
+                    </button> */}
+                </div>
+                <div className="mt-3 space-y-1 px-2">
+                  {userNavigation.map((item) => (
+                    <Disclosure.Button
+                      key={item.name}
+                      as={Link}
+                      to={item.href}
+                      className="block rounded-md py-2 px-3 text-base font-medium text-gray-500 hover:bg-gray-50 hover:text-gray-900">
+                      {item.name}
+                    </Disclosure.Button>
+                  ))}
+                </div>
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+
+      {children}
+
+      <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
+        <p className="py-8 text-base text-gray-400 border-t border-gray-200">
+          Travelermain.com is unofficial fan site for Genshin Impact Game. All
+          the Genshin impact related content and materials is copyrighted by
+          miHoYo.
+        </p>
+      </div>
+    </>
+  )
+}
