5268ac40a3553edf8ed4c73b6d9eb3e7799ef66e
Jody Geraldo <ajody.geraldo@gmail.com>
2022-02-07 17:24:34 +0700

    app layout

---
+++ app/routes/__app.tsx
@@ -0,0 +1,29 @@
+import { json, LoaderFunction, Outlet, useLoaderData } from 'remix'
+import invariant from 'tiny-invariant'
+
+import AppsLayout from '~/components/AppsLayout'
+import { requireUserId } from '~/services/auth.server'
+import { getUserData } from '~/utils/db/user.server'
+
+interface LoaderData {
+  email: string
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const id = await requireUserId(request)
+  const userData = await getUserData(id)
+
+  invariant(userData, "User data doesn't exist. WHO ARE YOU?")
+
+  return json({ email: userData.email }, { status: 200 })
+}
+
+export default function AppLayout() {
+  const { email } = useLoaderData<LoaderData>()
+
+  return (
+    <AppsLayout email={email}>
+      <Outlet />
+    </AppsLayout>
+  )
+}
