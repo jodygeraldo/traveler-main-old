e25fdc66995f5a1f5fc537a03ba80a053be2a9c8
Jody Geraldo <ajody.geraldo@gmail.com>
2022-02-07 17:20:59 +0700

    routes-gen

--- app/routes.d.ts
+++ app/routes.d.ts
@@ -1,48 +1,34 @@
 declare module "routes-gen" {
   export type RouteParams = {
-    "/character": {};
-    "/character/:characterName": { characterName: string };
-    "/character/:characterName/edit-with-inventory": { characterName: string };
-    "/character/:characterName/edit-with-inventory/items": { characterName: string };
-    "/character/:characterName/edit-manual": { characterName: string };
-    "/inventory": {};
-    "/inventory/ascension-boss-material": {};
-    "/inventory/talent-boss-material": {};
-    "/inventory/common-material": {};
-    "/inventory/local-specialty": {};
-    "/inventory/ascension-gem": {};
-    "/inventory/talent-book": {};
-    "/inventory/crown": {};
     "/logout": {};
     "/signup": {};
-    "/": {};
-    "/others": {};
-    "/toggle": {};
-    "/weekly": {};
     "/login": {};
+    "/": {};
+    "/character": {};
+    "/character/tracker": {};
+    "/character/": {};
+    "/character/:characterName": { characterName: string };
+    "/character/:characterName/items": { characterName: string };
+    "/inventory": {};
+    "/handbook": {};
+    "/handbook/others": {};
+    "/handbook/weekly": {};
   };
 
   export function route<
     T extends
-      | ["/character"]
-      | ["/character/:characterName", RouteParams["/character/:characterName"]]
-      | ["/character/:characterName/edit-with-inventory", RouteParams["/character/:characterName/edit-with-inventory"]]
-      | ["/character/:characterName/edit-with-inventory/items", RouteParams["/character/:characterName/edit-with-inventory/items"]]
-      | ["/character/:characterName/edit-manual", RouteParams["/character/:characterName/edit-manual"]]
-      | ["/inventory"]
-      | ["/inventory/ascension-boss-material"]
-      | ["/inventory/talent-boss-material"]
-      | ["/inventory/common-material"]
-      | ["/inventory/local-specialty"]
-      | ["/inventory/ascension-gem"]
-      | ["/inventory/talent-book"]
-      | ["/inventory/crown"]
       | ["/logout"]
       | ["/signup"]
-      | ["/"]
-      | ["/others"]
-      | ["/toggle"]
-      | ["/weekly"]
       | ["/login"]
+      | ["/"]
+      | ["/character"]
+      | ["/character/tracker"]
+      | ["/character/"]
+      | ["/character/:characterName", RouteParams["/character/:characterName"]]
+      | ["/character/:characterName/items", RouteParams["/character/:characterName/items"]]
+      | ["/inventory"]
+      | ["/handbook"]
+      | ["/handbook/others"]
+      | ["/handbook/weekly"]
   >(...args: T): typeof args[0];
 }
