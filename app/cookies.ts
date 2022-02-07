3c8e05f1ab0e664c0a5955055e68cdeae3d80961
Jody Geraldo <ajody.geraldo@gmail.com>
2022-02-07 17:26:48 +0700

    view pref cookie

--- app/cookies.ts
+++ app/cookies.ts
@@ -1,6 +1,13 @@
 import { createCookie } from 'remix'
 
-export const userThemePref = createCookie('user-theme-pref', {
+export const userThemePrefs = createCookie('user-theme-pref', {
+  path: '/',
+  sameSite: 'lax',
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',
+})
+
+export const userViewPrefs = createCookie('user-view-pref', {
   path: '/',
   sameSite: 'lax',
   httpOnly: true,
