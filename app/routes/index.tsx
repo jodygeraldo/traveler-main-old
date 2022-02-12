130b8a5efcf12bfbc0d59d6c11896398a6af487c
Jody Geraldo <ajody.geraldo@gmail.com>
2022-02-12 20:10:32 +0700

    fix wrong import

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Link, LoaderFunction, redirect } from 'remix'
+import { LoaderFunction, redirect } from 'remix'
 
 export const loader: LoaderFunction = () => {
   return redirect('/handbook')
